1021673e981d8e499be8987ed1bc10676eeb6cf3
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-03-14 18:04:58 -0400

    complex api 2 noun definition picture

--- README.md
+++ README.md
@@ -1,22 +1,11 @@
-# 📊 Project: Complex API 2
+# 📊 Project: Complex API 2: Looking up a noun which results in definition and the photo
 
 ### Goal: Use data returned from one api to make a request to another api and display the data returned
 
-### How to submit your code for review:
+### I completed the challenge: 5
 
-- Fork and clone this repo
-- Create a new branch called answer
-- Checkout answer branch
-- Push to your fork
-- Issue a pull request
-- Your pull request description should contain the following:
-  - (1 to 5 no 3) I completed the challenge
-  - (1 to 5 no 3) I feel good about my code
-  - Anything specific on which you want feedback!
+### I feel good about my code: 5
 
-Example:
-```
-I completed the challenge: 5
-I feel good about my code: 4
-I'm not sure if my constructors are setup cleanly...
-```
+## Tech used:
+# This is built using object oriented JS, HTML, and CSS!
+![nounIMG](images/Thumbnail.png)
--- js/main.js
+++ js/main.js
@@ -1,29 +1,40 @@
-let noun = document.querySelector('p')
+let nounElement = document.querySelector('#noun') //getting an element from the dom; dom is the document object model
 const button = document.querySelector('button')
+let definition = document.querySelector('#definition')
+let picture = document.querySelector('#picture')
 button.addEventListener("click", () =>{ //addevent is a function bc it has ()
-   const userPick = document.querySelector('input').value // changed from let to const bc this does not change when its in the function
-   fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${noun}?key=12c31f33-db0f-4c16-b7d2-047e3c830294`)
-     .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
-     .then(res => {
-       console.log(res)
-       for (let i = 0; i < res.results.length; i++) {
-         if (res.results[i].title === noun) { //loop through results; if the title of that result = user pick then print out open crawl of that result
-           console.log(res.results[i].noun)
-           fetch(`http://api.thenounproject.com?key=963c261c5ee94aa19ff11aab74981429&text=${res.}`)
-             .then(res => res.json())
-             .then(res => {
-               console.log(res.contents.translated)
-               noun.innerHTML = res.contents.translated
-             })
-           .catch(err => {
-             console.log(err)
-           })
-         }
-      }
+  let noun = nounElement.value; //noun eleement is thee input value and we are accessingthe value; value is what they typed
+  console.log('word:',noun)
+
+
+
+  let url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${noun}?key=12c31f33-db0f-4c16-b7d2-047e3c830294` // noun is put in, format URL in a separate way so the word comes right after the JSON
+  console.log(url)
+  fetch(url)
+  .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
+  .then(res => {
+    console.log(res[0].shortdef[0])
+    definition.innerHTML = res[0].shortdef[0] //portion of the res that had the result with the definition in it
+
+    let pixabayUrl = `https://pixabay.com/api/?key=15489800-0d402c1fa288dd4e9772fa4c8&q=${noun}`
+    console.log('pixabayUrl:', pixabayUrl)
+    fetch(pixabayUrl)
+    .then(res => res.json())
+    .then(res => {
+
+      // a360c02802fb4889bfeb1ac23094299e key
+      // c48f61cee32742ccac5c4bf717d55cbf secret
+
+      console.log(res.hits[0].webformatURL)
+      picture.src = res.hits[0].webformatURL
     })
-    .catch(err =>{
+    .catch(err => {
       console.log(err)
     })
+  })
+  .catch(err =>{
+    console.log(err)
+  })
 })
 
 
@@ -31,30 +42,30 @@ button.addEventListener("click", () =>{ //addevent is a function bc it has ()
 
 
 
-    //
-    //
-    // .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
-    // .then(res => {
-    //   console.log(res)
-    //
-    //
-    //
-    //   fetch('http://api.thenounproject.com?key=963c261c5ee94aa19ff11aab74981429')
-    //       .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
-    //       .then(res => {
-    //   console.log(table)
-    //
-    //   .catch(err => {
-    //         console.log(err)
-    //       })
-    //     })
+//
+//
+// .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
+// .then(res => {
+//   console.log(res)
+//
+//
+//
+//   fetch('http://api.thenounproject.com?key=963c261c5ee94aa19ff11aab74981429')
+//       .then(res => res.json()) // parse nasaSites as JSON (can be res.text() for plain nasaSites)
+//       .then(res => {
+//   console.log(table)
+//
+//   .catch(err => {
+//         console.log(err)
+//       })
+//     })
 
 
 //     .catch(err => {
 //       console.log(err)
 //     })
 // })
-})
+// })
 
 
 // https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=your-api-key
